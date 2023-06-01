61877f0f6aff893367e5df7e87cb77465a4ee8c7
Raul-Lozano-Garcia <rlozanogarcia97@gmail.com>
2023-06-01 17:58:58 +0200

    Cambio de nombre de nueva_rama a main

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -5,7 +5,6 @@ import Searchbar from './Searchbar';
 import Tipos from './Tipos';
 
 const { useContext } = React;
-//BORRAR ESTE COMENTARIO
 
 function Header({ pokemons, onSearch, fetchPokemonsPorTipo, fetchPokemonsFav }) {
 
